905a3b087354d308ec53ef6ba78fe916b5934839
Marques-Brenno <brennomarques04@gmail.com>
2026-05-28 04:50:06 -0300

    fase 1.1
    criando os campos novo e antigo

--- scripts/fase1_1/fase_1_1.gd
+++ scripts/fase1_1/fase_1_1.gd
@@ -6,8 +6,6 @@ var audio_instrucao = preload("res://sprites/audios/fase1_1/instrucao.mp3")
 var audio_acerto: AudioStream #= preload("res://pasta_dos_audios/acerto.mp3")
 var audio_erro: AudioStream # = preload("res://pasta_dos_audios/erro.mp3")
 
-audio_instrucao = load("res://sprites/audios/fase1_1/instrucao.mp3")
-
 func _ready() -> void:
 	# O resto do código continua exatamente igual!
 	$Avatar.mudar_fala(
