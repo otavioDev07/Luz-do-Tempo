75d1e84fbdf76369bfe9e4e7a44cf3674965ec83
Otávio Neto <netootavio223@outlook.com>
2026-05-30 23:47:04 -0300

    fix: letra arrastável

--- scripts/fase1.2/letra_arrastável.gd
+++ scripts/fase1.2/letra_arrastável.gd
@@ -4,9 +4,10 @@ func _ready() -> void:
 	mouse_filter = Control.MOUSE_FILTER_STOP
 
 func _get_drag_data(_at_position):
+	if not owner.pode_interagir:
+		return null
 	var preview = Label.new()
 	preview.text = text
 	preview.label_settings = load("res://fonts/fontefase1.2.tres")
 	set_drag_preview(preview)
-	
 	return text
