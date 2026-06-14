0dbb301dfa4f31ea4e6b56b8e56497b36db7091a
Otávio Neto <netootavio223@outlook.com>
2026-06-14 12:29:57 -0300

    alteração de link

--- scripts/menu/fase_1_completado.gd
+++ scripts/menu/fase_1_completado.gd
@@ -13,7 +13,7 @@ func _process(delta: float) -> void:
 
 
 func _on_botao_continuar_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/fase2/fase2.tscn")
+	get_tree().change_scene_to_file("res://scenes/fase2/fase_2.tscn")
 
 
 func _on_botao_continuar_mouse_entered() -> void:
