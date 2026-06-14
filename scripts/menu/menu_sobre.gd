663d7051a8c42661639822703d0bbc202c523c7f
Otávio Neto <netootavio223@outlook.com>
2026-06-14 12:45:24 -0300

    inseri o audio do sobre e alterei o texto

--- scripts/menu/menu_sobre.gd
+++ scripts/menu/menu_sobre.gd
@@ -1,4 +1,5 @@
 extends Control
+@onready  var tocador_audio = $sobre
 
 
 func _on_botao_voltar_pressed() -> void:
@@ -7,3 +8,8 @@ func _on_botao_voltar_pressed() -> void:
 
 func _on_botao_voltar_mouse_entered() -> void:
 	$voltar.play()
+
+
+func _on_rich_text_label_mouse_entered() -> void:
+	if tocador_audio != null:
+		tocador_audio.play()
