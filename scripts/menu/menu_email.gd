c8e6acb042470bf5a60dbf30c2884c939aba1a42
Marques-Brenno <brennomarques04@gmail.com>
2026-06-14 17:42:49 -0300

    menu

--- scripts/menu/menu_email.gd
+++ scripts/menu/menu_email.gd
@@ -29,14 +29,6 @@ func _on_botao_continuar_mouse_exited() -> void:
 	tween.tween_property($botao_continuar, "scale", Vector2(1.0, 1.0), 0.15)
 
 
-func _on_botao_voltar_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/menu/menu.tscn")
-
-
-func _on_botao_voltar_mouse_entered() -> void:
-	$voltar.play()
-
-
 func _on_imput_email_text_changed(new_text: String) -> void:
 	$digitando.stop()
 	$digitando.play()
